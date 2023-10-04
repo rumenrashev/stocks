dc85dbcbb0323a3681a9be26fd973b025ac3c553
Rumen Rashev <57558028+rumenrashev@users.noreply.github.com>
2023-10-04 12:37:44 +0300

    initial commit

---
+++ stocks/stocks/src/test/java/com/github/rumenrashev/stocks/StocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rumenrashev.stocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
